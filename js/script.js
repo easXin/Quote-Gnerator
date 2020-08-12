4a67cebdb30f7d6575cb18d879351037f34bbf40
easXin <codingericwang@gmail.com>
2020-08-11 23:46:05 -0700

    that's it for today

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 // Get Quote From API
 async function getQuote(){
+    // loading()
     const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
     /* because of async  const is not going to be set undtil
         required action is finished 
@@ -10,45 +16,27 @@ async function getQuote(){
         const response = await fetch(proxyUrl + apiUrl);
         // awaite for setting response above in json format
         const data = await response.json();
-        console.log(data);
+        if(data.quoteAuthor===''){
+            authorText.innerText="Unknown"
+        }else{
+            authorText.innerText=data.quoteAuthor;
+        }
+        if(data.quoteText.length>120){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove("long-quote");
+        }
+        quoteText.innerText = data.quoteText;
+        // stop loader and show quote 
+        // complete();
     } catch (error) {
-        getQuote();
-        console.log('whoops, no quote ',error);
+        // getQuote();
     }
 }
 
- getQuote();
+getQuote();
 /*
     Deal with Acess control allow origin Err
 
     Call proxy api first, then call quote api
 */
-
-
-// Get Quote From API
-// async function getQuote() {
-//     loading();
-//     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
-//     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-//     try {
-//         const response = await fetch(proxyUrl + apiUrl);
-//         const data = await response.json();
-//         // If Author is blank, add 'Unknown'
-//         if (data.quoteAuthor === '') {
-//             authorText.innerText = 'Unknown';
-//         } else {
-//             authorText.innerText = data.quoteAuthor;
-//         }
-//         // Reduce font size for long quotes
-//         if (data.quoteText.length > 120) {
-//             quoteText.classList.add('long-quote');
-//         } else {
-//             quoteText.classList.remove('long-quote');
-//         }
-//         quoteText.innerText = data.quoteText;
-//         // Stop Loader, Show Quote
-//         complete();
-//     } catch (error) {
-//         getQuote();
-//     }
-// }
